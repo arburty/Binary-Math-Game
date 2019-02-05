d381411d98e0bfbc47033e94431ebd331db30c04
austin.burt <austin.burt@homeadvisor.com>
2019-02-05 13:16:57 -0700

    fixed file linking

--- binaryFun.sh
+++ binaryFun.sh
@@ -1,1 +1,82 @@
-/Users/austin.burt/bin/binaryFun.sh
+#! /bin/bash
+
+declare -r PROGRAM=$(basename $0)
+
+USAGE() {
+    echo "ERROR: $PROGRAM: I don't know how but you mucked up"
+    exit 1
+}
+
+get_random() {
+    seconds=$(date +%s)
+    rndm=$(($seconds/$RANDOM%2))
+    echo -n $rndm
+}
+
+build4() {
+    str=""
+    for i in {1..4}
+    do
+        str="$str"$(get_random)
+    done
+    echo $str
+}
+getValue() {
+    if [[ -z "$1" ]]; then
+        USAGE
+    fi
+
+    let total=0
+    while read byte
+    do
+        total=$(($total*2))
+        if [[ $byte == "1" ]]
+        then
+            total=$(($total+1))
+        fi
+    done < <(grep -o . <<< $1)
+    echo "$total"
+}
+
+play() {
+    correct=0
+    wrong=0
+    while true
+    do
+        bytes=$(build4)
+        value=$(getValue $bytes)
+        echo "$bytes"
+        read input
+        if [[ $input == "q" ]]; then
+            echo "you put in $input"
+            exit 1
+        elif [[ $input -eq $value ]]
+        then
+            echo "NICE!"
+        else
+            echo "NOPE."
+        fi
+    done
+
+}
+
+main() {
+    read input
+    if [[ $input == "no" ]]; then
+        echo "Welp. Bye then"
+    elif [[ $input == "yes" ]]
+    then
+        echo "Good Luck! Enter 'q' to quit"
+        play
+    else
+        echo "-- Must press 'y' or 'n'"
+        main
+    fi
+}
+
+echo "Welcome! See how many binary strings you can correctly value."
+echo "Are you ready to play? (yes or no)"
+main
+
+exit 0
+new
